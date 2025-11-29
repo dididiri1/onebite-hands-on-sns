8d7734ac7144a923212a13d26621cfbe4f9dd20e
dididiri1 <dididiri1@naver.com>
2025-11-29 17:44:28 +0900

    docs: Zustand를 이용한 전역 상태관리 - 전역 상태 관리와 Zustand - Zustand 기본 사용법 1 - Zustand 기본 사용법 2

---
+++ section02/src/components/counter/controller.tsx
@@ -0,0 +1,20 @@
+import { useDecreaseCount, useIncreaseCount } from "@/store/count";
+import { Button } from "../ui/button";
+
+export default function Controller() {
+  const increase = useIncreaseCount();
+  const decrease = useDecreaseCount();
+
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          decrease();
+        }}
+      >
+        -
+      </Button>
+      <Button onClick={increase}>+</Button>
+    </div>
+  );
+}
