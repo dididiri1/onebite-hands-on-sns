ea6ca919818c3b16b52375e8b4d5e4940d870c49
dididiri1 <dididiri1@naver.com>
2025-11-30 10:59:25 +0900

    docs: Zustand를 이용한 전역 상태관리 - Zustand 미들웨어 1 - Zustand 미들웨어 2

--- section02/src/components/counter/controller.tsx
+++ section02/src/components/counter/controller.tsx
@@ -7,13 +7,7 @@ export default function Controller() {
 
   return (
     <div>
-      <Button
-        onClick={() => {
-          decrease();
-        }}
-      >
-        -
-      </Button>
+      <Button onClick={decrease}>-</Button>
       <Button onClick={increase}>+</Button>
     </div>
   );
--- section02/src/store/count.ts
+++ section02/src/store/count.ts
@@ -1,32 +1,82 @@
 import { create } from "zustand";
+import {
+  combine,
+  subscribeWithSelector,
+  persist,
+  createJSONStorage,
+  devtools,
+} from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
-type Store = {
-  count: number;
-  actions: {
-    increase: () => void;
-    decrease: () => void;
-  };
-};
+// type Store = {
+//   count: number;
+//   actions: {
+//     increase: () => void;
+//     decrease: () => void;
+//   };
+// };
 
-export const useCountStore = create<Store>((set, get) => ({
-  count: 0,
-  actions: {
-    increase: () => {
-      set((store) => {
-        return {
-          count: store.count + 1,
-        };
-      });
+export const useCountStore = create(
+  devtools(
+    persist(
+      subscribeWithSelector(
+        immer(
+          combine({ count: 0 }, (set, get) => ({
+            actions: {
+              increaseOne: () => {
+                set((state) => {
+                  state.count += 1;
+                });
+              },
+              decreaseOne: () => {
+                set((state) => {
+                  state.count -= 1;
+                });
+              },
+            },
+          })),
+        ),
+      ),
+      {
+        name: "countStore",
+        partialize: (store) => ({
+          count: store.count,
+        }),
+        storage: createJSONStorage(() => sessionStorage),
+      },
+    ),
+    {
+      name: "countStore",
     },
-    decrease: () => {
-      set((store) => {
-        return {
-          count: store.count - 1,
-        };
-      });
-    },
+  ),
+);
+
+useCountStore.subscribe(
+  (store) => store.count,
+  (count, prevCount) => {
+    console.log(count, prevCount);
   },
-}));
+);
+
+// export const useCountStore = create<Store>((set, get) => ({
+//   count: 0,
+//   actions: {
+//     increase: () => {
+//       set((store) => {
+//         return {
+//           count: store.count + 1,
+//         };
+//       });
+//     },
+//     decrease: () => {
+//       set((store) => {
+//         return {
+//           count: store.count - 1,
+//         };
+//       });
+//     },
+//   },
+// }));
 
 export const useCount = () => {
   const count = useCountStore((store) => store.count);
@@ -34,11 +84,11 @@ export const useCount = () => {
 };
 
 export const useIncreaseCount = () => {
-  const increase = useCountStore((store) => store.actions.increase);
+  const increase = useCountStore((store) => store.actions.increaseOne);
   return increase;
 };
 
 export const useDecreaseCount = () => {
-  const decrease = useCountStore((store) => store.actions.decrease);
+  const decrease = useCountStore((store) => store.actions.decreaseOne);
   return decrease;
 };
