5533078214ece3356ab10006d8e57f0161e2d77e
dididiri1 <dididiri1@naver.com>
2026-01-01 17:47:56 +0900

    docs: 섹션 6. 인증 처리하기 - 로그인 구현하기 - 소셜 로그인 구현하기 - 인증 에러 처리하기 1

--- onebite-log/src/types.ts
+++ onebite-log/src/types.ts
@@ -1,3 +1,10 @@
 import { type Database } from "./database.types";
 
 export type PostEnitiy = Database["public"]["Tables"]["post"]["Row"];
+
+export type useMutationCallback = {
+  onSuccess?: () => void;
+  onError?: (error: Error) => void;
+  onMutate?: () => void;
+  onSettled?: () => void;
+};
